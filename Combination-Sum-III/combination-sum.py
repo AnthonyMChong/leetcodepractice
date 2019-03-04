b34cb64aac7a45f0cbe3199a6ceeb036f9ff3077
AnthonyMChong <Anthonymchong@gmail.com>
2019-03-03 22:10:36 -0800

    fixed format of output... still missing part of solution

--- Combination-Sum-III/combination-sum.py
+++ Combination-Sum-III/combination-sum.py
@@ -1,17 +1,17 @@
 import pdb
-def findCombinations( k , n , startingnumber = 0):   # all combinations of 3 numbers that will add up to n
-    solList = []
-    if ( (startingnumber > n)):
-        return None #soultion is not viable
-    if (k == 1):
-        return n    # when we only have 1 space it is equal to the number
-    for testitem in range(startingnumber , n):
-        possibleSol = testitem + 1    # starting higher to prevent duplicates in each entry
-        leftover = n - possibleSol  # the leftover sum
-        returnlist = findCombinations( k-1 , leftover , possibleSol)
-        if returnlist is not None:
-            solList.append(possibleSol)
-    return solList
+# def findCombinations( k , n , startingnumber = 0):   # all combinations of 3 numbers that will add up to n
+#     solList = []
+#     if ( (startingnumber > n)):
+#         return None #soultion is not viable
+#     if (k == 1):
+#         return n    # when we only have 1 space it is equal to the number
+#     for testitem in range(startingnumber , n):
+#         possibleSol = testitem + 1    # starting higher to prevent duplicates in each entry
+#         leftover = n - possibleSol  # the leftover sum
+#         returnlist = findCombinations( k-1 , leftover , possibleSol)
+#         if returnlist is not None:
+#             solList.append(possibleSol)
+#     return solList
 
 def twoSum (n , start): #test function where the sum  n consists of start and another int that is larger
     if (start > n):
@@ -20,18 +20,26 @@ def twoSum (n , start): #test function where the sum  n consists of start and an
         return n - start
 
 def twoSumRec (k , n , start = 0): #test function where the sum  n consists of start and another int that is larger
+    print "running twoSumRec with:" , k , "  ", n
     retList = []
     if (start > n or k < 1):
         return None
     if (k == 1):
         return n    # when we only have 1 space it is equal to the number
-    else :
+    else :  ## we need to recurse some more 
         retList.append(start)
-        recReturn = twoSumRec(k -1 , n - start , start)
+        #for 
+        recReturn = twoSumRec(k -1 , n - start , start+1)
         if recReturn is not None:
-            retList.append(recReturn)
+            print start , " " , recReturn
+            if isinstance(recReturn, list ):#format correctly if list
+                for recReturnEl in recReturn:
+                    retList.append(recReturnEl)
+            else:   #if it is just an int
+                retList.append(recReturn)
         else:
             return None
+    return retList
 
 def twoSumRecHelper (k , n ):
     retList = []
@@ -44,6 +52,7 @@ def twoSumRecHelper (k , n ):
 
 
 
-answer = twoSumRecHelper( 2 , 3 )
+answer = twoSumRecHelper( 3 , 9 )
 print "our soulution:"
+
 print answer
