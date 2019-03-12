e43e083c181208b014e693309dfcefa06b7c3103
Anthony Chong <anthonymchong@gmail.com>
2019-03-12 11:47:45 -0700

    setting up majority element problem

---
+++ majority-element/majority-element.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def majorityElement( self , inlist ):
+        #print inlist
+        return inlist
+
+if __name__ == "__main__":
+    mylist = [2,2,1,1,1,2,2]
+    mySolution = Solution()
+    print mySolution.majorityElement( mylist )
