2b575e9f1dc380cd63c4b745c9fbab092a032106
Anthony Chong <anthonymchong@gmail.com>
2019-03-13 16:49:15 -0700

    completed majority-element problem

--- majority-element/majority-element.py
+++ majority-element/majority-element.py
@@ -1,9 +1,17 @@
 class Solution(object):
     def majorityElement( self , inlist ):
         #print inlist
-        return inlist
+        inlist.sort()
+        print inlist
+        nelements = len( inlist ) / 2
+        for checkmaj in  range ( 0 , len(inlist) - 1):
+            if checkmaj + nelements >= len(inlist):
+                print "no majority elements"
+                return None
+            elif inlist[checkmaj + nelements] == inlist[checkmaj]:
+                return inlist[checkmaj]
 
 if __name__ == "__main__":
-    mylist = [2,2,1,1,1,2,2]
+    mylist = [2,2,1,1,1,2,2, 2, 1 ,1,3,1 ,1 ]
     mySolution = Solution()
     print mySolution.majorityElement( mylist )
