956fc2ab53b5704e369c2cd0e64276980855d39a
Anthony Chong <anthonymchong@gmail.com>
2019-03-17 22:26:58 -0700

    have more direction and almost have list to binary tree. need to compare to cracking the coding interview methods for binary tree

---
+++ flip-binary-tree-to-match-preorder-traversal/flip-binary-tree.py
@@ -0,0 +1,8 @@
+from mybintree import *
+
+testlist = [1,2,3,4,5]
+testtree = binTree(testlist)
+print "\n checking the tree: "
+print testtree.head.value
+print testtree.head.leftnode.value
+print testtree.head.rightnode.value
--- flip-binary-tree-to-match-preorder-traversal/mybintree.py
+++ flip-binary-tree-to-match-preorder-traversal/mybintree.py
@@ -7,13 +7,21 @@ class node:
         #return this
 
 
-class binTree: # returns head of the binary tree we are creating
+class binTree(object): # returns head of the binary tree we are creating
     head = None
     def __init__( self, nodelist ):
-        isinstance(nodelist, int):
+        if isinstance(nodelist, int):
             self.head = node(nodelist)
             return
         nodelist.sort()
-        head = node(nodelist[len(nodelist)/2])
+        self.head = node(nodelist[len(nodelist)/2])
+        self.head.leftnode = self.recinsert( nodelist[0:(len(nodelist)/2)] )
+        self.head.rightnode = self.recinsert( nodelist[(len(nodelist)/2)+1:] )
+        return
         #for nodeEl in nodelist:
+    def recinsert ( self , nodelist):
+        # if len(node)
+        
+        print nodelist
+        return node( nodelist[len(nodelist)/2] )
         
